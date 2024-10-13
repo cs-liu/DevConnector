6a2855af98b80cd395032ec3ee5eb4adb8982c00
csliu <3064189521@qq.com>
2024-10-13 13:37:08 +0800

    finish github repos

--- models/Profile.js
+++ models/Profile.js
@@ -1,113 +1,126 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProfileSchema = new mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    company: {
-        type: String
+  /*
+  user: 这是一个 ObjectId 类型的字段，表示 MongoDB 的 _id。该字段是引用类型（ref: "user"），
+  与 User 模型关联（代表用户的个人资料与用户的关系），用于在数据库中创建外键引用。
+
+  experience, education 是嵌套数组，每个元素代表一段工作经历或教育经历
+  social 是一个包含社交媒体链接的嵌套对象，用户可以存储自己在各个平台的个人主页链接
+
+  嵌套对象和嵌套数组的区别
+  嵌套对象是一个包含多个字段的单个对象，而嵌套数组是包含多个相同结构对象的数组。
+  嵌套对象适合表示单一的复杂字段，嵌套数组则适合表示多个类似的条目，比如用户的多个工作经历或教育背景。
+  如果字段只包含一个复杂的值，如一个地址或一组联系信息，使用嵌套对象。
+  如果字段包含多个相同类型的条目，如多段工作经历或多条技能记录，使用嵌套数组。
+  */
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
       },
-      website: {
-        type: String
+      company: {
+        type: String,
+        required: true,
       },
       location: {
-        type: String
-      },
-      status: {
         type: String,
-        required: true
       },
-      skills: {
-        type: [String],
-        required: true
-      },
-      bio: {
-        type: String
-      },
-      githubusername: {
-        type: String
-      },
-      experience: [
-        {
-          title: {
-            type: String,
-            required: true
-          },
-          company: {
-            type: String,
-            required: true
-          },
-          location: {
-            type: String
-          },
-          from: {
-            type: Date,
-            required: true
-          },
-          to: {
-            type: Date
-          },
-          current: {
-            type: Boolean,
-            default: false
-          },
-          description: {
-            type: String
-          }
-        }
-      ],
-      education: [
-        {
-          school: {
-            type: String,
-            required: true
-          },
-          degree: {
-            type: String,
-            required: true
-          },
-          fieldofstudy: {
-            type: String,
-            required: true
-          },
-          from: {
-            type: Date,
-            required: true
-          },
-          to: {
-            type: Date
-          },
-          current: {
-            type: Boolean,
-            default: false
-          },
-          description: {
-            type: String
-          }
-        }
-      ],
-      social: {
-        youtube: {
-          type: String
-        },
-        twitter: {
-          type: String
-        },
-        facebook: {
-          type: String
-        },
-        linkedin: {
-          type: String
-        },
-        instagram: {
-          type: String
-        }
-      },
-      date: {
+      from: {
         type: Date,
-        default: Date.now
-      }
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,13 @@
+/**
+ * express: 用于创建API路由。
+ * gravatar: 用于根据邮箱自动生成用户头像URL，Gravatar是一个基于电子邮件地址生成头像的服务。
+ * bcryptjs: 一个常用的哈希加密库，用来加密和验证密码。
+ * jsonwebtoken: 用于生成JWT令牌，用来进行用户身份验证。
+ * config: 读取应用程序配置文件中的JWT密钥（jwtSecret）。
+ * express-validator: 用于对用户输入进行验证，确保请求体中的数据符合要求。
+ * User: Mongoose的用户模型，负责与MongoDB数据库交互。
+ */
+
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
@@ -53,9 +63,11 @@ router.post(
       });
 
       //Encrypt password
+      //bcrypt.genSalt(10)用于生成加密盐值。
+      //盐的作用是增加哈希的随机性，即使两个用户使用相同的密码，生成的哈希值也不同，增加破解难度。
       const salt = await bcrypt.genSalt(10);
 
-      user.password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hash(password, salt); //加密用户密码
 
       //save to the database
       await user.save();
@@ -69,6 +81,11 @@ router.post(
         },
       };
 
+      /**
+       * JWT令牌是如何使用的？
+       * JWT令牌包含用户的信息，通常在请求头中作为Bearer令牌发送给服务器，
+       * 用于验证用户身份。服务器可以通过解析JWT令牌来验证请求是否合法，进而提供相应的服务。
+       */
       //签署令牌
       jwt.sign(
         playload, //传入有效负载
